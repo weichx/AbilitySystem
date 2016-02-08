27921e1956c0f33de3f9d8cc179c3a7c4654fb09
weichx <matthew.weichselbaum@gmail.com>
2016-02-07 22:51:38 -0500

    organized files

--- ArenaGameManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[RequireComponent(typeof(TimeManager))]
-[RequireComponent(typeof(FactionManager))]
-public class ArenaGameManager : MonoBehaviour {
-
-    public void Start() { }
-
-}
