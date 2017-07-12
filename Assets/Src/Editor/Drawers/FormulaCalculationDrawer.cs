88e9c6470e9253c2eb978aa83a61815db93e58c9
Tobias Rosen <tobias.rosen@axsh.net>
2017-07-12 21:13:48 +0900

    editor: create custom drawer for fomula calculation component

---
+++ Assets/Src/Editor/Drawers/FormulaCalculationDrawer.cs
@@ -0,0 +1,48 @@
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using System;
+using EntitySystem;
+using Intelligence;
+
+[PropertyDrawerFor(typeof(FormulaCalculation))]
+public class FormulaCalculationDrawer : PropertyDrawerX {
+
+    private SerializedPropertyX rootProperty;
+    private SerializedPropertyX listRoot;
+    private bool initialized;
+    private ListRenderer listRenderer;
+
+    public override void OnGUI(SerializedPropertyX source, GUIContent label) {
+        Initialize(source);
+        listRenderer.Render();
+    }
+
+    private void Initialize(SerializedPropertyX source) {
+        if (!initialized) {
+            var tmp = source.GetParent;
+            while (tmp != null) {
+                rootProperty = tmp;
+                tmp = rootProperty.GetParent;
+            }
+            listRoot = source["modifiers"];
+
+            listRenderer = new ListRenderer();
+            listRenderer.SetTargetProperty(rootProperty, listRoot);
+            listRenderer.SetSearchBox(CreateSearchBox);
+            initialized = true;
+        }
+    }
+
+    private SearchBox CreateSearchBox() {
+        Type targetType = rootProperty["contextType"].GetValue<Type>();
+        var searchSet = Reflector.FindSubClasses(typeof(Modifier));
+        searchSet = searchSet.FindAll((modifierType) => {
+                var dummy = Activator.CreateInstance(modifierType) as Modifier;
+                return dummy.GetContextType().IsAssignableFrom(targetType);
+            });
+        return new SearchBox(null, searchSet, listRenderer.AddListItem, "Add Modifier", "Modifiers");
+    }
+}
