3bc1caa49db7955b62fc55b8f197a8e14bcb1202
Tobias Rosen <tobias.rosen@axsh.net>
2017-07-17 09:51:29 +0900

    editor: only set the list of pointable methods once

--- Assets/Src/Editor/Drawers/MethodPointers/MethodPointerDrawer.cs
+++ Assets/Src/Editor/Drawers/MethodPointers/MethodPointerDrawer.cs
@@ -7,41 +7,11 @@ using System.Collections.Generic;
 public class MethodPointerDrawerX : PropertyDrawerX {
 
     protected List<MethodPointer> pointableMethods;
-
-    // Debug button for easier testing of formula outputs
-    // public void CreateTestButton() {
-    //     if (GUILayout.Button("Test formula")) {
-    //         for (int i = 0; i < listRoot.ChildCount; i++) {
-    //             var child = listRoot.GetChildAt(i);
-    //             if (child.Type == typeof(DamageFormula)) {
-    //                 var value = (int)child.FindProperty("baseDamage").Value;
-    //                 var formula = child.FindProperty("damageFormula");
-    //                 var target = (CharacterCreator)child.FindProperty("TargetCharacter").Value;
-    //                 Delegate d = Reflector.FindDelegateWithSignature(formula.Value.ToString());
-    //                 Func<SingleTargetContext, float, float> fn = d as Func<SingleTargetContext, float, float>;
-    //                 var ctx = (SingleTargetContext)target.Create().GetContext();
-    //                 Debug.Log(fn(cntx, value));
-    //             }
-    //         }
-    //     }
-    // }
+    private bool initialized;
 
     public override void OnGUI(SerializedPropertyX source, GUIContent label) {
-        Type sourceType = source.type;
-        if (sourceType.IsGenericType) {
-            Type[] genericTypes = sourceType.GetGenericArguments();
-            Type[] args = new Type[genericTypes.Length - 1];
-            for (int i = 0; i < (genericTypes.Length - 1); i++) {
-                args[i] = genericTypes[i];
-            }
-            pointableMethods = Reflector.FindMethodPointersWithAttribute(typeof(Pointable), genericTypes[genericTypes.Length - 1], args);
-        }
-        else {
-            pointableMethods = Reflector.FindMethodPointersWithAttribute(typeof(Pointable), typeof(void), Type.EmptyTypes);
-        }
-        if (source.Value == null) {
-            source.Value = Activator.CreateInstance(source.type);
-        }
+        Initialize(source);
+
         var displayList = new GUIContent[pointableMethods.Count + 1];
         displayList[0] = new GUIContent("-- None --");
         for (int i = 1; i < displayList.Length; i++) {
@@ -51,10 +21,30 @@ public class MethodPointerDrawerX : PropertyDrawerX {
         if (signature.Value == null) signature.Value = displayList[0].text;//
         int idx = Array.FindIndex(displayList, content => {
             return content.text == signature.Value as string;
-        }); 
+        });
         if (idx == -1) idx = 0;
         int newIdx = EditorGUILayout.Popup(label, idx, displayList);
         signature.Value = displayList[newIdx].text;
     }
-}
 
+    private void Initialize(SerializedPropertyX source) {
+        if(!initialized) {
+            Type sourceType = source.type;
+            if (sourceType.IsGenericType) {
+                Type[] genericTypes = sourceType.GetGenericArguments();
+                Type[] args = new Type[genericTypes.Length - 1];
+                for (int i = 0; i < (genericTypes.Length - 1); i++) {
+                    args[i] = genericTypes[i];
+                }
+                pointableMethods = Reflector.FindMethodPointersWithAttribute(typeof(Pointable), genericTypes[genericTypes.Length - 1], args);
+            }
+            else {
+                pointableMethods = Reflector.FindMethodPointersWithAttribute(typeof(Pointable), typeof(void), Type.EmptyTypes);
+            }
+            if (source.Value == null) {
+                source.Value = Activator.CreateInstance(source.type);
+            }
+            initialized = true;
+        }
+    }
+}
--- Assets/Src/Game/Formulas/Modifiers.cs
+++ Assets/Src/Game/Formulas/Modifiers.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class ContextModifier : Modifier<Context> {
     public int test;
-    public MethodPointer<float> formula;
+    public MethodPointer<Context, float> formula;
     public override void ApplyModifier(ref float value) {
         value *= 2;
     }
@@ -13,17 +13,17 @@ public class ContextModifier : Modifier<Context> {
 
 public class NoModifier : Modifier<SingleTargetContext> {
     public int test;
-    public MethodPointer<SingleTargetContext> formula;
+    public MethodPointer<SingleTargetContext, float, float> formula;
     public override void ApplyModifier(ref float value) {
         value += 10;
     }
 }
 
 public class OneModifier : Modifier<MultiPointContext> {
-    public MethodPointer<SingleTargetContext, float, float> formula;
+    public MethodPointer<MultiPointContext, float, float> formula;
 }
 
 public class TwoModifier : Modifier<DirectionalContext> {
-    public MethodPointer <SingleTargetContext, float, float, float> formula;
+    public MethodPointer <DirectionalContext, float, float, float> formula;
 }
 
